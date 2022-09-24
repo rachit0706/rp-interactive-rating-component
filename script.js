e6c21ac1af554fd7d342f856ffdf482e6f81d2a1
Rachit Patel <rachit0706@gmail.com>
2022-09-24 16:40:49 +0530

    Implemented thankyou state

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const ratingSelectArea = document.querySelector(".select-rating");
+const ratingSubmitBtn = document.querySelector(".submit");
+const ratingState = document.querySelector(".rating-state");
+const thankyouState = document.querySelector(".thankyou-state");
+const yourRating = document.querySelector(".your-rating");
 
-function displayRatingButton(){
-    for(let i = 1; i <= 5; i++) {
+function displayRatingButton() {
+    for (let i = 1; i <= 5; i++) {
         const starDiv = document.createElement("span");
         starDiv.id = `star-${i}`;
         starDiv.className = `star-button`;
-        starDiv.textContent=`${i}`;
-        
+        starDiv.textContent = `${i}`;
+
         ratingSelectArea.append(starDiv);
     }
 }
@@ -15,13 +19,24 @@ displayRatingButton();
 
 let selectedStar;
 
-ratingSelectArea.addEventListener('click', function(e) {
+ratingSelectArea.addEventListener('click', function (e) {
     selectedStar = Number(e.target.id.split('-')[1]);
     console.log(selectedStar)
-    
-    for(let i = 1; i <= 5; i++) {
+    yourRating.textContent = `Your rating is ${selectedStar} out of 5`;
+
+    for (let i = 1; i <= 5; i++) {
         const highligtedButton = document.getElementById(`star-${i}`);
-        
+
         highligtedButton.style.backgroundColor = i === selectedStar ? "hsl(25, 97%, 53%)" : "hsl(216, 38%, 31%)";
     }
 })
+
+ratingSubmitBtn.addEventListener('click', function () {
+    if(!selectedStar){
+        alert("Please Select a Rating!!");
+    }else{
+        ratingState.style.display = "none";
+        thankyouState.style.display = "block";
+        thankyouState.style.display = "flex";
+    }
+});
